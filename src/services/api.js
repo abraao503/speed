7120754ed90ec6574b9da47726b734d5622ecbe8
Abraão <abraao503@gmail.com>
2024-07-23 14:17:48 -0400

    refactor: remove unnecessary headers in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
-  withCredentials: true,
-  headers: {
-    "ngrok-skip-browser-warning": true,
-  },
 });
 
 export const openApi = axios.create({
