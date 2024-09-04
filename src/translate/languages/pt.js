bbff60ad66a1f9be727d9d4ec9660c51d2d569cd
Abraão <abraao503@gmail.com>
2024-09-03 21:20:37 -0400

    chore: Update Portuguese translation for "tickets" field in table

--- src/translate/languages/pt.js
+++ src/translate/languages/pt.js
@@ -834,7 +834,7 @@ const messages = {
         table: {
           name: "Nome",
           color: "Cor",
-          tickets: "Registros Tagdos",
+          tickets: "Tickets associados",
           actions: "Ações",
         },
         buttons: {
