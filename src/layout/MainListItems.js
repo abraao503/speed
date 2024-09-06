27b2c8fc52f6126bcf17d58fe305293eb2709c1b
Matheus Antunes <matheus.antunes@tarkuss.com.br>
2024-09-05 22:34:30 -0400

    fix kanban delay

--- src/layout/MainListItems.js
+++ src/layout/MainListItems.js
@@ -150,17 +150,16 @@ const MainListItems = (props) => {
   }, [searchParam]);
 
   useEffect(() => {
-    async function fetchData() {
-      const companyId = user.companyId;
-      const planConfigs = await getPlanCompany(undefined, companyId);
-
-      setShowCampaigns(planConfigs.plan.useCampaigns);
-      setShowKanban(planConfigs.plan.useKanban);
-      setShowOpenAi(planConfigs.plan.useOpenAi);
-      setShowIntegrations(planConfigs.plan.useIntegrations);
-      setShowSchedules(planConfigs.plan.useSchedules);
-      setShowInternalChat(planConfigs.plan.useInternalChat);
-      setShowExternalApi(planConfigs.plan.useExternalApi);
+    function fetchData() {
+      const { company: { plan } } = user 
+            
+      setShowCampaigns(plan.useCampaigns);
+      setShowKanban(plan.useKanban);
+      setShowOpenAi(plan.useOpenAi);
+      setShowIntegrations(plan.useIntegrations);
+      setShowSchedules(plan.useSchedules);
+      setShowInternalChat(plan.useInternalChat);
+      setShowExternalApi(plan.useExternalApi);
     }
     fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
