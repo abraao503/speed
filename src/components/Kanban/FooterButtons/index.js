056f82e295061adc1ad0ffa832cf8908a8a2cf4a
Matheus Antunes <matheus.antunes@tarkuss.com.br>
2024-10-07 20:42:04 -0400

    add: implement note on kanban

--- src/components/Kanban/FooterButtons/index.js
+++ src/components/Kanban/FooterButtons/index.js
@@ -4,6 +4,7 @@ import { BookOpen, Calendar } from "react-feather";
 import WhatsAppIcon from "@material-ui/icons/WhatsApp";
 import ScheduleModal from "../../ScheduleModal";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import ContactNotesDialog from "../../ContactNotesDialog";
 
 const useStyles = makeStyles(theme => ({
     container: {
@@ -16,11 +17,20 @@ export default function FooterButtons({ticket}) {
     const classes = useStyles();
     const color = "#4d4d4d"
     const [scheduleModalOpen, setScheduleModalOpen] = useState(false);
+    const [contactNotesModal, setContactNotesModal] = useState(false)
     const history = useHistory();
 
     const handleOpenScheduleModal = () => {
         setScheduleModalOpen(true);
     };
+
+    const handleOpenContactNotesModal = () => {
+        setContactNotesModal(true)
+    }
+
+    const handleCloseContactNotesModal = () => {
+        setContactNotesModal(false)
+    }
 
     const handleCloseScheduleModal = () => {
         setScheduleModalOpen(false);
@@ -31,29 +41,32 @@ export default function FooterButtons({ticket}) {
     };
 
     return (
-        <div className={classes.container}>
-            <Tooltip title="Anotações">
-            <IconButton size="small">
-                <BookOpen color={color} size={17} />
-            </IconButton>
-            </Tooltip>
-            <Tooltip title="Criar agendamento">
-                <IconButton onClick={handleOpenScheduleModal} size="small">
-                    <Calendar color={color} size={17} />
+        <>
+            <ContactNotesDialog modalOpen={contactNotesModal} onClose={handleCloseContactNotesModal} ticket={ticket} />
+            <div className={classes.container}>
+                <Tooltip title="Anotações">
+                <IconButton onClick={handleOpenContactNotesModal} size="small">
+                    <BookOpen color={color} size={17} />
                 </IconButton>
-            </Tooltip>
-            <Tooltip title="Ir para conversa">   
-                <IconButton onClick={handleSelectTicket} size="small">
-                    <WhatsAppIcon style={{color: color}} fontSize="small" />
-                </IconButton>
-            </Tooltip>
+                </Tooltip>
+                <Tooltip title="Criar agendamento">
+                    <IconButton onClick={handleOpenScheduleModal} size="small">
+                        <Calendar color={color} size={17} />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="Ir para conversa">   
+                    <IconButton onClick={handleSelectTicket} size="small">
+                        <WhatsAppIcon style={{color: color}} fontSize="small" />
+                    </IconButton>
+                </Tooltip>
 
-            <ScheduleModal
-                open={scheduleModalOpen}
-                onClose={handleCloseScheduleModal}
-                aria-labelledby="form-dialog-title"
-                contactId={ticket.contact.id}
-            />
-        </div>
+                <ScheduleModal
+                    open={scheduleModalOpen}
+                    onClose={handleCloseScheduleModal}
+                    aria-labelledby="form-dialog-title"
+                    contactId={ticket.contact.id}
+                />
+            </div>
+        </>
     )
 }
